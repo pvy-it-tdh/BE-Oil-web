a5cb46facf55e9d834a71e7f5c764459ff772318
pvy-it-tdh <phucvy107@gmail.com>
2024-07-04 10:02:31 +0700

    add model

--- src/main/java/com/example/demo/model/ShoppingCart.java
+++ src/main/java/com/example/demo/model/ShoppingCart.java
@@ -2,11 +2,23 @@ package com.example.demo.model;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 public class ShoppingCart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cartId;
+
     @ManyToOne
     @JoinColumn(name ="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+    private Date createAt;
+
 }
