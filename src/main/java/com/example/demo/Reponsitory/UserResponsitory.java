c9f1425847114772d57b5730edcb8b4feac1d244
pvy-it-tdh <phucvy107@gmail.com>
2024-07-15 15:42:00 +0700

    update

--- src/main/java/com/example/demo/Reponsitory/UserResponsitory.java
+++ src/main/java/com/example/demo/Reponsitory/UserResponsitory.java
@@ -2,7 +2,9 @@ package com.example.demo.Reponsitory;
 
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserResponsitory extends JpaRepository<User,Long> {
-
+        User findByUsername(String username);
 }
