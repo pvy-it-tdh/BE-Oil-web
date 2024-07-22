922635ce574f24f1fa3e6a240cc4b0bff76e1f24
pvy-it-tdh <phucvy107@gmail.com>
2024-07-22 15:12:07 +0700

    update

---
+++ src/main/java/com/example/demo/JWT/JwtRequestFilter.java
@@ -0,0 +1,31 @@
+package com.example.demo.JWT;
+
+import com.example.demo.Services.UserService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+                                    throws ServletException, IOException {
+        final String requestTokenHeader=request.getHeader("Authorization");
+        String username=null;
+        String jwtToke=null;
+
+
+    }
+}
